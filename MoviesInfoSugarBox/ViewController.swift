c0b5627933d43395f18ad7307ed4468998cede57
keerthidevipriya <keerthidevipriya@gmail.com>
2023-11-21 12:32:10 +0530

    Initial project

---
+++ MoviesInfoSugarBox/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MoviesInfoSugarBox
+//
+//  Created by Keerthi Devipriya(kdp) on 21/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
+
