f6c7d8edb969ff541bfd822676e91debf960bbce
keerthidevipriya <keerthidevipriya@gmail.com>
2023-11-21 19:49:17 +0530

    Added the tabbar

--- MoviesInfoSugarBox/App/Controllers/MainTabBarController.swift
+++ MoviesInfoSugarBox/App/Controllers/MainTabBarController.swift
@@ -15,13 +15,23 @@ class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         let item1 = HomeViewController.init(viewModel: ListViewModel())
-        let icon1 = UITabBarItem(title: "SugarBox", image: UIImage(systemName: "paperplane"), selectedImage: UIImage(systemName: "paperplane.fill"))
+        let icon1 = UITabBarItem(title: "Home", image: UIImage(systemName: "bell"), selectedImage: UIImage(systemName: "bell.fill"))
         item1.tabBarItem = icon1
-        let controllers = [item1]
+        
+        let item2 = ViewController()
+        let icon2 = UITabBarItem(title: "Movies", image: UIImage(systemName: "heart"), selectedImage: UIImage(systemName: "heart.fill"))
+        item2.tabBarItem = icon2
+        
+        let item3 = ViewController()
+        let icon3 = UITabBarItem(title: "Shows", image: UIImage(systemName: "bell"), selectedImage: UIImage(systemName: "bell.fill"))
+        item3.tabBarItem = icon3
+        let controllers = [item1, item2, item3]
         self.viewControllers = controllers
     }
     
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         return true
     }
+    
+    
 }
